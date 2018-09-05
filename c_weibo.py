72faaf2c86f9ddd8c26e5b8c85d2084a3e80ddf7
Archean-wang <1091876908@qq.com>
2018-09-05 08:14:24 +0800

    no cookies

--- c_weibo.py
+++ c_weibo.py
@@ -1,45 +1,46 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+# coding：utf-8
 
 import re
 import grequests
 import requests
 from lxml import etree
 
+
 class WeiboComment(object):
-    def __init__(self, weibo_url, cookies, username):
+    def __init__(self, weibo_url,  username):
         self.urls = []
         self.user = username
         self.weibo_url = weibo_url
-        self.cookies = dict(i.split('=') for i in cookies.split(';'))
+        # self.cookies = dict(i.split('=') for i in cookies.split(';'))
         self.headers = {
-            'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-            'accept-encoding':'gzip, deflate, br',
+            'user-agent': 'spider',# 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8'
         }
 
-    def getbase(self):
-        a = requests.get(self.weibo_url, headers=self.headers, cookies=self.cookies)
-        totalcomment = int(re.findall(r'<em>(\d+)<\\/em>', a.text)[1])
-        if totalcomment%20 ==0:
-            totalpage = totalcomment//20
+    def _base(self):
+        a = requests.get(self.weibo_url, headers=self.headers)
+        comments = int(re.findall(r'<em>(\d+)<', a.text)[0])
+        if comments % 20 == 0:
+            pages = comments//20
         else:
-            totalpage = totalcomment//20 + 1
-        weibo_id = re.findall(r'\\"id=(\d+)&filter', a.text)[0]
-        print('总共%s页,%s评论' % (totalpage, totalcomment))
-        for page in range(1, totalpage+1):
-            base_url = 'https://weibo.com/aj/v6/comment/big?ajwvr=6&id=%s&filter=all&page=' % (weibo_id)
-            c_url = base_url + str(page)
-            self.urls.append(c_url)
+            pages = comments//20 + 1
+        weibo_id = re.findall(r'"id=(\d+)&filter', a.text)[0]
+        print('总共%s页,%s评论' % (pages, comments))
+        for page in range(1, pages+1):
+            url = f'https://weibo.com/aj/v6/comment/big?ajwvr=6&id={weibo_id}&filter=all&page={page}'
+            self.urls.append(url)
 
     @staticmethod
     def exception_handler(request, exception):
         try:
-            return requests.get(request.url, headers=self.headers, cookies=self.cookies)
+            return requests.get(request.url, headers=request.headers)
         except:
             print(exception, request.url)
 
     def getcomments(self):
-        tasks = (grequests.get(url, headers=self.headers, cookies=self.cookies) for url in self.urls)
+        tasks = (grequests.get(url, headers=self.headers) for url in self.urls)
         bs = grequests.map(tasks, size=10, exception_handler=self.exception_handler)
         for b in bs:
             if b:
@@ -48,17 +49,17 @@ class WeiboComment(object):
                 c = etree.HTML(c_html.encode('unicode_escape'))
                 for i in c.xpath('//div[@class="WB_text"]'):
                     user, comment = i.xpath('string(.)').encode('utf-8').decode('unicode_escape').strip().split('：', maxsplit=1)
-                    #print(user, comment)
+                    print(f'{user}:{comment}')
                     if user == self.user:
-                        print('~~~~~~~~~~%s' % (comment))
+                        print(f'{user}:{comment}')
 
     def run(self):
-        self.getbase()
+        self._base()
         self.getcomments()
 
-if __name__=='__main__':
-    weibo_url = input('请输入微博链接:')
-    cookies = input('请输入cookies:')
+if __name__ == '__main__':
+    init_url = input('请输入微博链接:')
+    # cookies = input('请输入cookies:')
     username = input('请输入用户名:')
-    weibocomment = WeiboComment(weibo_url, cookies, username)
+    weibocomment = WeiboComment(init_url, username)
     weibocomment.run()
