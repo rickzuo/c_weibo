7b3481cf87e4fae946d4dd67a0a73342c06de246
Archean-wang <1091876908@qq.com>
2018-09-06 02:50:30 +0800

    use selenium

--- c_weibo.py
+++ c_weibo.py
@@ -1,33 +1,46 @@
 # !/usr/bin/env python3
 # coding：utf-8
 
+import time
 import re
 import grequests
 import requests
 from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class WeiboComment(object):
-    def __init__(self, weibo_url,  username):
+    def __init__(self, weibo_url,  user):
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
         self.urls = []
-        self.user = username
+        self.user = user
         self.weibo_url = weibo_url
-        # self.cookies = dict(i.split('=') for i in cookies.split(';'))
+        self.cookies = {}
         self.headers = {
-            'user-agent': 'spider',# 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8'
         }
 
     def _base(self):
-        a = requests.get(self.weibo_url, headers=self.headers)
-        comments = int(re.findall(r'<em>(\d+)<', a.text)[0])
+        self.driver.get(self.weibo_url)
+        time.sleep(5)
+        source = self.driver.page_source
+        _cookies = self.driver.get_cookies()
+        for cookie in _cookies:
+            self.cookies[cookie['name']] = cookie['value']
+        self.driver.close()
+        comments = int(re.findall(r'<em>(\d+)<', source)[0])
         if comments % 20 == 0:
             pages = comments//20
         else:
             pages = comments//20 + 1
-        weibo_id = re.findall(r'"id=(\d+)&filter', a.text)[0]
-        print('总共%s页,%s评论' % (pages, comments))
+        weibo_id = re.findall(r'"id=(\d+)&amp;filter', source)[0]
+        print(f'总共{pages}页,{comments}评论')
         for page in range(1, pages+1):
             url = f'https://weibo.com/aj/v6/comment/big?ajwvr=6&id={weibo_id}&filter=all&page={page}'
             self.urls.append(url)
@@ -35,12 +48,12 @@ class WeiboComment(object):
     @staticmethod
     def exception_handler(request, exception):
         try:
-            return requests.get(request.url, headers=request.headers)
+            return requests.get(request.url, headers=request.headers, cookies=request.cookies)
         except:
             print(exception, request.url)
 
     def getcomments(self):
-        tasks = (grequests.get(url, headers=self.headers) for url in self.urls)
+        tasks = (grequests.get(url, headers=self.headers, cookies=self.cookies) for url in self.urls)
         bs = grequests.map(tasks, size=10, exception_handler=self.exception_handler)
         for b in bs:
             if b:
@@ -49,7 +62,7 @@ class WeiboComment(object):
                 c = etree.HTML(c_html.encode('unicode_escape'))
                 for i in c.xpath('//div[@class="WB_text"]'):
                     user, comment = i.xpath('string(.)').encode('utf-8').decode('unicode_escape').strip().split('：', maxsplit=1)
-                    print(f'{user}:{comment}')
+                    # print(f'{user}:{comment}')
                     if user == self.user:
                         print(f'{user}:{comment}')
 
@@ -59,7 +72,6 @@ class WeiboComment(object):
 
 if __name__ == '__main__':
     init_url = input('请输入微博链接:')
-    # cookies = input('请输入cookies:')
     username = input('请输入用户名:')
-    weibocomment = WeiboComment(init_url, username)
-    weibocomment.run()
+    wc = WeiboComment(init_url, username)
+    wc.run()
