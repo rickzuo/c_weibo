f9b32f920d6e86b7e4d84b1c12b538fc1ca0a572
Archean_w <10918769080@qq.com>
2018-09-15 00:21:21 +0800

    fix log

--- run.py
+++ run.py
@@ -5,21 +5,22 @@ import logging
 from weibo import WeiboComment
 from settings import LOG_FILE, LOG_LEVEL
 
+_level = {'INFO': logging.INFO,
+          'DEBUG': logging.DEBUG,
+          'WARNING': logging.WARNING,
+          'ERROR': logging.ERROR
+          }
+_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+handler = logging.FileHandler(LOG_FILE)
+handler.setLevel(_level[LOG_LEVEL.upper()])
+formatter = logging.Formatter(_format)
+handler.setFormatter(formatter)
+logging.basicConfig(handlers=[handler], level=_level[LOG_LEVEL.upper()])
+logger = logging.getLogger('root')
+
 
 def main():
-    _level = {'INFO': logging.INFO,
-              'DEBUG': logging.DEBUG,
-              'WARNING': logging.WARNING,
-              'ERROR': logging.ERROR}
-    _format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    # logging.basicConfig(filename=LOG_FILE, format=_format, level=_level[LOG_LEVEL.upper()])
-    logger = logging.getLogger(__name__)
-    logger.setLevel(_level[LOG_LEVEL.upper()])
-    handler = logging.FileHandler(LOG_FILE)
-    handler.setLevel(_level[LOG_LEVEL.upper()])
-    formatter = logging.Formatter(_format)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+
     logger.info('-' * 150)
     wb = WeiboComment()
     wb.run()
